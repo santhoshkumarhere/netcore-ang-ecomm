4c26d2b8066148eb1a460cfef7d91aa63ab49b45
kumars9 <santhoshk.kumar@corteva.com>
2021-03-20 16:30:40 -0500

    git end of section 5

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,48 @@
+using API.Errros;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(42);
+            
+            if(thing == null)
+            {
+             return NotFound(new ApiResponse(404));
+            }
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+            var things = thing.ToString(); // does not exists
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+         [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequestParam(int id)
+        {
+            return Ok();
+        }
+    }
+}
--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,12 +9,12 @@ using System.Threading.Tasks;
 using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using API.Errros;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product>  _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -42,13 +42,18 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
             var product = await _productsRepo.GetEntityWithSpec(spec);
+            if(product == null)
+            return NotFound(new ApiResponse(400));
             return _mapper.Map<Product, ProductToReturnDto>(product);          
         }
-         [HttpGet("brands")]
+
+        [HttpGet("brands")]
         public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
         {
             var products = await _productBrandRepo.ListAllAsync();
