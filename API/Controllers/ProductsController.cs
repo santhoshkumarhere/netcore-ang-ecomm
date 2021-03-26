17a18248aeaf5e835c02a5c3245c917826b37901
santhosh <santhoshk.kumar@corteva.com>
2021-03-26 15:29:03 -0500

    end of section 6

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,6 +11,7 @@ using API.Dtos;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using API.Errros;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -34,11 +35,19 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<IReadOnlyList<ProductToReturnDto>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+            [FromQuery]ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+            var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+
+            var totalItems = await _productsRepo.CountAsync(countSpec);
+
             var products = await _productsRepo.ListAsync(spec);
-            return _mapper.Map<IReadOnlyCollection<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            var data = _mapper.Map<IReadOnlyCollection<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
         }
 
         [HttpGet("{id}")]
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -7,15 +7,31 @@ namespace Infrastructure.Data
 {
     public class  SpecificationEvaluator<TEntity> where TEntity : BaseEntity
     {
-        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> specification)
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
         {
             var query = inputQuery;
-            if(specification.Criteria != null)
+            
+            if(spec.Criteria != null)
             {
-                query = query.Where(specification.Criteria);
+                query = query.Where(spec.Criteria);
             }
 
-            query = specification.Includes.Aggregate(query, (current, include) => current.Include(include));
+            if(spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if(spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if(spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take); // paging should be after sorting
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
             
             return query;
         }
